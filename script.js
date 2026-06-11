e1256e64c3b1b0700804ea7d51f4f299042aa6ca
Keren R. Bell <keren.r.bell@gmail.com>
2026-06-11 14:42:14 +0300

    Interactivity for all form items

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-function setUpRadio() {
+
+function buildRabbit() {
+    const name = document.getElementById('nameField').value;
+    let results = document.getElementById('results');
+
+    results.innerHTML = "Your new rabbit, " + name + " , will arrive"
+}
+
+
+function setUp() {
+    const nameField = document.getElementById('nameField')
+    nameField.addEventListener('change', nameUpdate);
+
     document.querySelectorAll('input[name="rabbitColor"]').forEach(radio => {
         radio.addEventListener('change', radioUpdate);
     });
+    document.querySelectorAll('input[name="accessories"]').forEach(checkbox => {
+        checkbox.addEventListener('change', checkUpdate);
+    });
 }
 
+function nameUpdate(event) {
+    const nameDisplay = document.getElementById('rabbitName');
+    nameDisplay.innerHTML = event.target.value;
+}
+function checkUpdate(event) {
+    const matchingAccessory = document.getElementById(event.target.id + 'Img')
+    if (matchingAccessory.className = 'disabled') {
+        matchingAccessory.className = 'enabled'
+    } else {
+        matchingAccessory.className = 'disabled'
+    }
+}
 function radioUpdate(event) {
     const rabbitPreview = document.getElementById('mainRabbit');
     rabbitPreview.className = event.target.value;
