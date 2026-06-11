1f3af0587b772cd6405cc16dcc8f9c7a3c956e05
Keren R. Bell <keren.r.bell@gmail.com>
2026-06-11 14:18:45 +0300

    Working radios, CSS updates

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function myFunction() {
-    document.getElementById("mainRabbit").classList
+function myFunction(event) {
+    const rabbitPreview = document.getElementById('mainRabbit');
+
+    rabbitPreview.className = event.target.value;
 }
+
+document.querySelectorAll('input[name="rabbitColor"]').forEach(radio => {
+    radio.addEventListener('change', myFunction);
+});
